e2ec42068fd8498b2c9d3c12f25ef5baee4f9b13
Su-Shing Chen <shingyx.dev@gmail.com>
2023-09-09 12:16:44 +1000

    Decompose into more files and scripts (#11)

---
+++ src/interfaces.ts
@@ -0,0 +1,33 @@
+export enum ConflictAction {
+  REMOVE,
+  KEEP,
+}
+
+export interface IKeymapXml {
+  keymap: {
+    $: {
+      name: string;
+      version: string;
+      'disable-mnemonics': string;
+      parent?: string;
+    };
+    action: IKeymapXmlAction[];
+  };
+}
+
+export interface IKeymapXmlAction {
+  $: {
+    id: string;
+  };
+  'keyboard-shortcut'?: {
+    $: {
+      'first-keystroke': string;
+      'second-keystroke'?: string;
+    };
+  }[];
+  'mouse-shortcut'?: {
+    $: {
+      keystroke: string;
+    };
+  }[];
+}
